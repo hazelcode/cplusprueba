82f5956c171e4f7f66f3157cf0e4dd65c408009d
tacozyt <lacosaqhabla@gmail.com>
2021-10-11 11:04:44 -0500

    Casos para los operadores
    Ya la calculadora esta avanzada

--- cplusprueba/cplusprueba.cpp
+++ cplusprueba/cplusprueba.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hola!\n";
-    std::cout << "Estas viendo\n";
-    std::cout << "una prueba.\n";
+    // SIN USARSE POR LO MIENTRAS XD (Desde Lunes 11 de Octubre del 2021)
+    // std::cout << "Hola!\n";
+    // std::cout << "Estas viendo\n";
+    // std::cout << "una prueba.\n";
+
+    cout << "CALCULADORA" << endl << endl;
+    cout << "Escribe una operacion! Formatos: a+b | a-b | a*b | a/b"
+        << endl;
 }
